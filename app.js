c877e17f9d958b9bac074a997ce5236c65561bb5
J V <jaganvarah22@gmail.com>
2022-09-10 15:22:07 +0530

    update

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.static('./build'))
-
+ 
 const userdata = require('./src/Model/usermodel')
 const productdata = require('./src/Model/productmodel')
 
